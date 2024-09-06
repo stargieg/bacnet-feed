b6d37dcbbc70321993f49fc0d266150f31516571
Patrick Grimm <patrick@lunatiki.de>
2024-09-06 23:43:36 +0200

    luci-app-bacnetclient: update

--- luci-app-bacnet-client/htdocs/luci-static/resources/view/eventlist.js
+++ luci-app-bacnet-client/htdocs/luci-static/resources/view/eventlist.js
@@ -19,32 +19,56 @@ function createTable(data) {
     let tableData = [];
     data.list.forEach(row => {
 		let event_time_stamp_date = Date.parse(row.event_time_stamp);
-		let event_time_stamp_loc = new Date(event_time_stamp_date);
-		let event_time_stamp = event_time_stamp_loc.toLocaleDateString('de-DE') + ' ' + event_time_stamp_loc.toLocaleTimeString('de-DE')
+		let event_time_stamp;
+		let ack;
+		if (isNaN(event_time_stamp_date) == false) {
+			let event_time_stamp_loc = new Date(event_time_stamp_date);
+			event_time_stamp = event_time_stamp_loc.toLocaleDateString('de-DE') + ' ' + event_time_stamp_loc.toLocaleTimeString('de-DE');
+			if (row.acked_transitions_offnormal) {
+				ack =
+				E('span', { 'class': 'control-group' }, [
+					E('button', {
+						'class': 'cbi-button cbi-button-apply',
+						'click': ui.createHandlerFn(this, function() {
+							return callsetData(row.devid,row.object_type,row.object_instance,event_time_stamp_date,'offnormal');
+						})
+					}, 
+					_('Quit')),
+				]);
+			} else {
+				ack = "";
+			}
+		} else {
+			event_time_stamp = "";
+			ack = "";
+		}
 		let event_time_stamp_normal_date = Date.parse(row.event_time_stamp_normal);
-		let event_time_stamp_normal_loc = new Date(event_time_stamp_normal_date);
-		let event_time_stamp_normal = event_time_stamp_normal_loc.toLocaleDateString('de-DE') + ' ' + event_time_stamp_normal_loc.toLocaleTimeString('de-DE')
+		let event_time_stamp_normal;
+		let ack_normal;
+		if (isNaN(event_time_stamp_normal_date) == false) {
+			let event_time_stamp_normal_loc = new Date(event_time_stamp_normal_date);
+			event_time_stamp_normal = event_time_stamp_normal_loc.toLocaleDateString('de-DE') + ' ' + event_time_stamp_normal_loc.toLocaleTimeString('de-DE');
+			if (row.acked_transitions_offnormal) {
+				ack_normal =
+				E('span', { 'class': 'control-group' }, [
+					E('button', {
+						'class': 'cbi-button cbi-button-apply',
+						'click': ui.createHandlerFn(this, function() {
+							callsetData(row.devid,row.object_type,row.object_instance,event_time_stamp_normal_date,'fault');
+							callsetData(row.devid,row.object_type,row.object_instance,event_time_stamp_normal_date,'normal');
+							return;
+						})
+					}, 
+					_('Quit')),
+				]);
+			} else {
+				ack_normal = "";
+			}
+		} else {
+			event_time_stamp_normal = "";
+			ack_normal = "";
+		}
 
-		let ack =
-			E('span', { 'class': 'control-group' }, [
-				E('button', {
-					'class': 'cbi-button cbi-button-apply',
-					'click': ui.createHandlerFn(this, function() {
-						return callsetData(row.devid,row.object_type,row.object_instance,event_time_stamp_date,'offnormal');
-					})
-				}, 
-				_('Quit')),
-			]);
-		let ack_normal =
-			E('span', { 'class': 'control-group' }, [
-				E('button', {
-					'class': 'cbi-button cbi-button-apply',
-					'click': ui.createHandlerFn(this, function() {
-						return callsetData(row.devid,row.object_type,row.object_instance,event_time_stamp_normal_date,'normal');
-					})
-				}, 
-				_('Quit')),
-			]);
 		tableData.push([
             row.idx,
             row.devid,
